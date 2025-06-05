d4ccd06feafd67de1157300581d5315a845a29a2
Adam Hess <adam.hess@forthepeople.com>
2025-06-05 15:26:37 -0400

    removed pants init

--- pants_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd $(pwd)
-ROOTS=$(pants roots)
-python3 -c "print('PYTHONPATH=./' + ':./'.join('''${ROOTS}'''.replace(' ', '\\ ').split('\n')) + ':\$PYTHONPATH')" > .env
-pants export --py-resolve-format=symlinked_immutable_virtualenv --resolve=python-default
